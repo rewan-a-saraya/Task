cb02830bd968c2a5f6e783dc9a7c2ead511a5a44
Al forsan <rewansaraya@gmail.com>
2025-03-03 16:42:15 +0200

    add

---
+++ p.py
@@ -0,0 +1,33 @@
+#K-means Clustering
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+X,y = make_blobs(n_samples = 500,n_features = 2,centers = 3,random_state = 23)
+
+fig = plt.figure(0)
+plt.grid(True)
+plt.scatter(X[:,0],X[:,1])
+plt.show()
+
+# count.K-means Clustering
+
+
+k = 3
+
+clusters = {}
+np.random.seed(23)
+
+for idx in range(k):
+    center = 2 * (2 * np.random.random((X.shape[1],)) - 1)
+    points = []
+    cluster = {
+        'center': center,
+        'points': []
+    }
+
+    clusters[idx] = cluster
+
+clusters
