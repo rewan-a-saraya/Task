9e2b3844dcd342b83af94238cc1d6a71253d35d4
Al forsan <rewansaraya@gmail.com>
2025-03-17 23:31:40 +0200

    MongoDB

--- mdb3.py
+++ mdb3.py
@@ -1,12 +1,19 @@
-import os
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 from motor.motor_asyncio import AsyncIOMotorClient
+
+class Settings(BaseSettings):
+    MONGO_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
 
 app = FastAPI()
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-client = AsyncIOMotorClient(MONGO_URL)
+client = AsyncIOMotorClient(settings.MONGO_URL)
 db = client["mydatabase"]
 collection = db["users"]
 
@@ -16,7 +23,7 @@ class User(BaseModel):
 
 @app.post("/add_user/")
 async def add_user(user: User):
-    new_user = await collection.insert_one(user.model_dump())  # Use model_dump()0
+    new_user = await collection.insert_one(user.model_dump())
     return {"message": "User added", "user_id": str(new_user.inserted_id)}
 
 @app.get("/get_users/")
@@ -24,10 +31,4 @@ async def get_users():
     users = await collection.find().to_list(100)
     return users
 
-
-# uvicorn mdb2:app --reload
-
-
-
-
-
+# uvicorn mdb3:app --reload
