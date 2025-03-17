ab1eebac2bf86fe2a6ad9381b52466a1fb3e7789
Al forsan <rewansaraya@gmail.com>
2025-03-17 22:08:27 +0200

    MongoDB2

--- mdb2.py
+++ mdb2.py
@@ -1,10 +1,11 @@
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["mydatabase"]
 collection = db["users"]
@@ -18,7 +19,6 @@ async def add_user(user: User):
     new_user = await collection.insert_one(user.model_dump())  # Use model_dump()
     return {"message": "User added", "user_id": str(new_user.inserted_id)}
 
-
 @app.get("/get_users/")
 async def get_users():
     users = await collection.find().to_list(100)
@@ -26,4 +26,8 @@ async def get_users():
 
 
 # uvicorn mdb2:app --reload
+
+
+
+
 
